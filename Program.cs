10e53f3d3ffe05dda5fea216b0aed3119507a434
ADJ43 <adjse43@gmail.com>
2024-01-23 22:29:44 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderScript
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWords = 0;
+            string path = @"";  // Add desired file path
+            if(File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
+                foreach(string line in lines)
+                {
+                    string[] words = line.Split(' ');
+                    numberOfWords += words.Length;
+                    
+                }
+                Console.WriteLine("ready, the # of words is " + numberOfWords);
+            } else
+            {
+                Console.WriteLine("na sama lasatra");
+            }
+        }
+    }
+}
